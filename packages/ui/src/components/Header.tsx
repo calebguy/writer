8a813b4fc43cf023e6de0ff1e3bb2d30b6d8e6c2
caleb <caleb@syndicate.io>
2024-11-23 22:39:16 +0700

    ⚉

--- packages/ui/src/components/Header.tsx
+++ packages/ui/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { usePrivy } from "@privy-io/react-auth";
 import { useContext } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 import { WriterContext } from "../layouts/App.layout";
 import { cn } from "../utils/cn";
 import { Button, ButtonVariant } from "./Button";
@@ -8,17 +8,35 @@ import { Blob } from "./icons/Blob";
 
 export function Header() {
 	const { ready, authenticated, login, logout } = usePrivy();
-	// const navigate = useNavigate();
 	const location = useLocation();
+	const { address, id } = useParams();
 	const { writer } = useContext(WriterContext);
 	const isLoggedIn = ready && authenticated;
+
+	console.log({ writer, id, location });
+
+	let title = "Writer";
+	if (location.pathname === "/") {
+		title = "Writer";
+	} else if (writer) {
+		if (id) {
+			title = `${writer.title} / ${id}`;
+		} else {
+			title = writer.title;
+		}
+	}
+
+	let to = "/";
+	if (address && id) {
+		to = `/writer/${address}`;
+	}
 
 	return (
 		<div className="flex">
 			<div className="flex justify-between items-center w-full">
 				<div className="flex items-end gap-2">
 					<Link
-						to="/"
+						to={to}
 						className={cn(
 							"text-3xl active:-translate-x-[1px] active:translate-y-[1px]",
 							{
@@ -27,7 +45,7 @@ export function Header() {
 							},
 						)}
 					>
-						{location.pathname === "/" ? "Writer" : writer?.title}
+						{title}
 					</Link>
 				</div>
 				<Button
--- packages/ui/src/routes/Entry.tsx
+++ packages/ui/src/routes/Entry.tsx
@@ -1,7 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { useContext, useEffect } from "react";
 import { useParams } from "react-router";
+import type { Hex } from "viem";
+import { WriterContext } from "../layouts/App.layout";
+import { getWriter } from "../utils/api";
 
 export default function Entry() {
 	const { address, id } = useParams();
+	const { setWriter } = useContext(WriterContext);
+
+	const { data } = useQuery({
+		queryFn: () => getWriter(address as Hex),
+		queryKey: ["get-writer", address],
+		enabled: !!address,
+	});
+	useEffect(() => {
+		if (data) {
+			setWriter(data);
+		}
+	}, [data, setWriter]);
+
 	console.log(address, id);
 	return (
 		<div>
