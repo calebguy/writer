9ff8d0612fe1d182be696480a02f3185a448b8f3
caleb <caleb@syndicate.io>
2025-09-01 10:50:19 -0500

    input

--- packages/web/src/components/CreateWriterInput.tsx
+++ packages/web/src/components/CreateWriterInput.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { cn } from "@/utils/cn";
+import type { MDXEditorMethods } from "@mdxeditor/editor";
 import dynamic from "next/dynamic";
 import { useEffect, useRef, useState } from "react";
 
@@ -9,13 +10,17 @@ const MDX = dynamic(() => import("./markdown/MDX"), { ssr: false });
 export default function CreateWriterInput() {
 	const [hasFocus, setHasFocus] = useState(false);
 	const containerRef = useRef<HTMLDivElement>(null);
+	const [markdown, setMarkdown] = useState<string>("");
+	const editorRef = useRef<MDXEditorMethods>(null);
 
 	// Handle clicks inside or outside the container
 	useEffect(() => {
 		const handleClick = (event: MouseEvent) => {
 			if (containerRef.current) {
 				if (!containerRef.current.contains(event.target as Node)) {
-					setHasFocus(false);
+					if (markdown.trim() === "") {
+						setHasFocus(false);
+					}
 				} else {
 					setHasFocus(true);
 				}
@@ -26,12 +31,13 @@ export default function CreateWriterInput() {
 		return () => {
 			document.removeEventListener("mousedown", handleClick);
 		};
-	}, []);
+	}, [markdown]);
 
 	// Handle escape key
 	useEffect(() => {
 		const handleKeyDown = (event: KeyboardEvent) => {
 			if (event.key === "Escape") {
+				editorRef.current?.setMarkdown("");
 				setHasFocus(false);
 			}
 		};
@@ -55,7 +61,8 @@ export default function CreateWriterInput() {
 				<span>+</span>
 			</div>
 			<MDX
-				markdown={""}
+				ref={editorRef}
+				markdown={markdown}
 				autoFocus
 				className={cn(
 					"group-hover:flex border-dashed bg-neutral-900 flex-col placeholder:text-green-300",
@@ -65,6 +72,7 @@ export default function CreateWriterInput() {
 					},
 				)}
 				placeholder="Create a Place"
+				onChange={setMarkdown}
 			/>
 		</div>
 	);
