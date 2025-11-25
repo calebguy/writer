bcae53e6ab414801e315b2dc2b6e8a3c8717d194
caleb <caleb@syndicate.io>
2025-11-25 10:48:47 -0500

    up

--- packages/web/src/components/WritersForManager.tsx
+++ packages/web/src/components/WritersForManager.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import { type Writer, deleteWriter, factoryCreate } from "@/utils/api";
+import {
+	type Writer,
+	deleteWriter,
+	factoryCreate,
+	getWritersByManager,
+} from "@/utils/api";
 import type { UserWithWallet } from "@/utils/auth";
-import { useMutation } from "@tanstack/react-query";
+import { POLLING_INTERVAL } from "@/utils/constants";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import dynamic from "next/dynamic";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import type { Hex } from "viem";
 import CreateInput from "./CreateInput";
 import { MarkdownRenderer } from "./MarkdownRenderer";
 const MDX = dynamic(() => import("./markdown/MDX"), { ssr: false });
@@ -13,12 +21,22 @@ export function WritersForManager({
 	writers,
 	user,
 }: { writers: Array<Writer>; user?: UserWithWallet }) {
+	const [isPolling, setIsPolling] = useState(false);
+
+	const address = user?.wallet.address;
+	const { data, refetch } = useQuery({
+		queryFn: () => getWritersByManager(address as Hex),
+		queryKey: ["get-writers", address],
+		enabled: !!address,
+		refetchInterval: isPolling ? POLLING_INTERVAL : false,
+	});
+
 	const { mutateAsync, isPending } = useMutation({
 		mutationFn: factoryCreate,
 		mutationKey: ["create-from-factory"],
 		onSuccess: () => {
-			// refetch();
-			// setIsPolling(true);
+			refetch();
+			setIsPolling(true);
 		},
 	});
 
@@ -27,6 +45,15 @@ export function WritersForManager({
 		mutationKey: ["delete-writer"],
 	});
 
+	useEffect(() => {
+		const isAllOnChain = data?.every((writer) => writer.createdAtHash);
+		if (isAllOnChain) {
+			setIsPolling(false);
+		} else if (!isPolling) {
+			setIsPolling(true);
+		}
+	}, [data, isPolling]);
+
 	return (
 		<div
 			className="grid gap-2"
@@ -34,8 +61,8 @@ export function WritersForManager({
 				gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
 			}}
 		>
-			{!!user && <CreateInput placeholder="Create a Place for Writing" />}
-			{writers?.map((writer) => (
+			{!!user && <CreateInput placeholder="Create a Place" />}
+			{(data ?? writers)?.map((writer) => (
 				<Link
 					href={`/writer/${writer.address}`}
 					key={writer.address}
