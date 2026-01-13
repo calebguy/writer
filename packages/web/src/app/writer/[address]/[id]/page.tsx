3789c59b71166acc29786881efa40bf36be4f3aa
caleb <caleb@syndicate.io>
2026-01-13 01:16:31 -0500

    fix loading

--- packages/web/src/app/writer/[address]/[id]/page.tsx
+++ packages/web/src/app/writer/[address]/[id]/page.tsx
@@ -68,6 +68,16 @@ export default function EntryPage({
 					<div className="h-4 bg-neutral-700 rounded w-full" />
 					<div className="h-4 bg-neutral-700 rounded w-full" />
 					<div className="h-4 bg-neutral-700 rounded w-5/6" />
+					<div className="h-4 bg-neutral-700 rounded w-full" />
+					<div className="h-4 bg-neutral-700 rounded w-2/3" />
+					<div className="h-4 bg-neutral-700 rounded w-full" />
+					<div className="h-4 bg-neutral-700 rounded w-4/5" />
+				</div>
+				<div className="flex items-end mt-3">
+					<div className="space-y-1">
+						<div className="h-4 bg-neutral-700 rounded w-32" />
+						<div className="h-4 bg-neutral-700 rounded w-28" />
+					</div>
 				</div>
 			</div>
 		);
