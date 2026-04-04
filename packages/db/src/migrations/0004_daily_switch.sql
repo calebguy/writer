acaaadeb73da5db9f97a488cce77f9b4694d1045
caleb <caleb@syndicate.io>
2026-04-04 13:12:52 -0500

    fix migration

--- packages/db/src/migrations/0004_daily_switch.sql
+++ packages/db/src/migrations/0004_daily_switch.sql
@@ -17,7 +17,6 @@ ALTER TABLE "entry" ADD CONSTRAINT "entry_created_at_transaction_id_relay_tx_id_
 ALTER TABLE "entry" ADD CONSTRAINT "entry_deleted_at_transaction_id_relay_tx_id_fk" FOREIGN KEY ("deleted_at_transaction_id") REFERENCES "public"."relay_tx"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "entry" ADD CONSTRAINT "entry_updated_at_transaction_id_relay_tx_id_fk" FOREIGN KEY ("updated_at_transaction_id") REFERENCES "public"."relay_tx"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "writer" ADD CONSTRAINT "writer_transaction_id_relay_tx_id_fk" FOREIGN KEY ("transaction_id") REFERENCES "public"."relay_tx"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "public"."relay_tx" ALTER COLUMN "status" SET DATA TYPE text;--> statement-breakpoint
-DROP TYPE "public"."request_status";--> statement-breakpoint
-CREATE TYPE "public"."request_status" AS ENUM('PENDING', 'SUBMITTED', 'CONFIRMED', 'ERROR');--> statement-breakpoint
-ALTER TABLE "public"."relay_tx" ALTER COLUMN "status" SET DATA TYPE "public"."request_status" USING "status"::"public"."request_status";
+ALTER TYPE "public"."request_status" ADD VALUE IF NOT EXISTS 'ERROR';--> statement-breakpoint
+UPDATE "relay_tx" SET "status" = 'SUBMITTED' WHERE "status" = 'PROCESSED';--> statement-breakpoint
+UPDATE "relay_tx" SET "status" = 'ERROR' WHERE "status" IN ('PAUSED', 'ABANDONED');
