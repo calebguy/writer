9bbe09b52307aea5872cf15b928a4e34cf797f18
caleb <caleb@syndicate.io>
2026-04-06 14:20:31 -0500

    correct og

--- packages/utils/constants.ts
+++ packages/utils/constants.ts
@@ -1,3 +1,3 @@
 export const MAX_TITLE_LENGTH = 500;
 export const MAX_CONTENT_LENGTH = 500_000;
-export const OG_IMAGE_URL = "https://writer.place/OG.png";
+export const OG_IMAGE_URL = "https://www.writer.place/images/OG.png";
